fdb599d93435aca4e59c9f74e59d8cb7cbbf19b1
Andrei Cervantes <andrei29.cervantes@gmail.com>
2024-06-30 18:19:35 +0800

    Fixed structure of the website

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AboutUs() {
+    return (
+        <div className='text-center'>About Us</div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import MainNavbar from '../components/MainNavbar';
+import Landing from '../components/Landing';
+import AboutUs from '../components/AboutUs';
+import Products from '../components/Products';
+import Services from '../components/Services';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+import './Home.css';
 
 export default function Home() {
     return (
-        <MainNavbar />
+        <div className='body-container'>
+            <MainNavbar />
+            <Landing />
+            <AboutUs />
+            <Products />
+            <Services />
+            <Contact />
+            <Footer />
+        </div>
+        
     )
 }
