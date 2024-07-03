edd2c5de0ae55238b92254475f48aeeabef4b4b6
Andrei Cervantes <andrei29.cervantes@gmail.com>
2024-07-03 20:12:49 +0800

    Finished contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,10 @@ import './Contact.css';
 
 export default function Contact() {
     return (
-        <div className='contact-container d-flex '>
+        <div className='contact-container'>
             <h2 className='syne'>contact us</h2>
-            <div className='contact-info-container'>
-                <div className='map-address-container'>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.7726093668625!2d121.03100407510746!3d14.668841985825669!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b6d5c0888283%3A0xde7fa7551eedaa93!2sMetro%20North%20Medical%20Center%20Hospital!5e0!3m2!1sen!2sph!4v1719994078913!5m2!1sen!2sph" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
+            <div className='contact-info-container justify-center justify-md-space-between'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.7726093668625!2d121.03100407510746!3d14.668841985825669!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b6d5c0888283%3A0xde7fa7551eedaa93!2sMetro%20North%20Medical%20Center%20Hospital!5e0!3m2!1sen!2sph!4v1719994078913!5m2!1sen!2sph" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div className='contact-info'>
                     <div className='contacts'>
                         <div>(0912) 345 6789 (02) 123 4567</div>
